779ad6f622a101883360a57855b098398918c3c0
KREN1 <30318710+krenfix15@users.noreply.github.com>
2020-10-09 14:37:18 +0300

    Update Hwcal_chans.cs

--- SREC FILE UPDATER/Hwcal_chans.cs
+++ SREC FILE UPDATER/Hwcal_chans.cs
@@ -9,90 +9,90 @@ namespace HWCAL
     /// </summary>
     public enum HWCAL_CHANS_ENUM
     {
-        Hwcal_chan_PORT0_CHAN0_idx,           /* 0U */
-        Hwcal_chan_I_A_TLF_5V_STBY_idx,       /* 1U */
-        Hwcal_chan_I_A_INV_CUR_U_idx,         /* 2U */
-        Hwcal_chan_PORT0_CHAN3_idx,           /* 3U */
-        Hwcal_chan_I_A_CW_TEMP1_idx,          /* 4U */
-        Hwcal_chan_I_A_GMR_SIN_idx,           /* 5U */
-        Hwcal_chan_I_A_XMR_VDIAG_idx,         /* 6U */
-        Hwcal_chan_PORT0_CHAN7_idx,           /* 7U */
-        Hwcal_chan_PORT1_CHAN0_idx,           /* 8U */
-        Hwcal_chan_I_A_TLF_5V_COM_idx,            /* 9U */
-        Hwcal_chan_I_A_INV_CUR_V_idx,             /* 10U */
-        Hwcal_chan_PORT1_CHAN3_idx,               /* 11U */
-        Hwcal_chan_I_A_CW_TEMP2_idx,              /* 12U */
-        Hwcal_chan_I_A_GMR_COS_idx,               /* 13U */
-        Hwcal_chan_I_A_CTRL_REV_IN_idx,           /* 14U */
-        Hwcal_chan_PORT1_CHAN7_idx,               /* 15U */
-        Hwcal_chan_PORT2_CHAN0_idx,               /* 16U */
+        Hwcal_chan_PORT0_idx,           /* 0U */
+        Hwcal_chan_I_A__STBY_idx,       /* 1U */
+        Hwcal_chan_I_A__U_idx,          /* 2U */
+        Hwcal_chan_PORT0_idx,           /* 3U */
+        Hwcal_chan_I_A_CW_idx,          /* 4U */
+        Hwcal_chan_I_A_idx,             /* 5U */
+        Hwcal_chan_I_idx,               /* 6U */
+        Hwcal_chan_PORT0_idx,           /* 7U */
+        Hwcal_chan_PORT1_idx,           /* 8U */
+        Hwcal_chan_I_A_TLF_idx,         /* 9U */
+        Hwcal_chan_I_A_V_idx,           /* 10U */
+        Hwcal_chan_PORT1_idx,           /* 11U */
+        Hwcal_chan_I_A_CW_idx,          /* 12U */
+        Hwcal_chan_I_A_GMR_COS,         /* 13U */
+        Hwcal_chan_I_A_CTRL_REV_IN_idx,       /* 14U */
+        Hwcal_chan_PORT3_idx,                 /* 15U */
+        Hwcal_chan_PORT2_CHAN0,               /* 16U */
         Hwcal_chan_I_A_TLF_5V_T1_idx,             /* 17U */
         Hwcal_chan_I_A_UZK_P_idx,                 /* 18U */
         Hwcal_chan_PORT2_CHAN3_idx,               /* 19U */
-        Hwcal_chan_I_A_CW_TEMP3_idx,              /* 20U */
-        Hwcal_chan_I_A_OP_TEMP2_idx,              /* 21U */
-        Hwcal_chan_I_A_PSA_DIAG_1_idx,            /* 22U */
-        Hwcal_chan_PORT2_CHAN7_idx,               /* 23U */
-        Hwcal_chan_I_A_OP_TEMP1_idx,              /* 24U */
-        Hwcal_chan_I_A_TLF_5V_T2_idx,             /* 25U */
-        Hwcal_chan_I_A_UZK_N_idx,                 /* 26U */
-        Hwcal_chan_I_A_INT_12V_idx,               /* 27U */
-        Hwcal_chan_I_A_CW_TEMP4_idx,              /* 28U */
+        Hwcal_chan_I_A_TEMP3_idx,              /* 20U */
+        Hwcal_chan_I_A_TEMP2_idx,              /* 21U */
+        Hwcal_chan_I_A_PSIAG_1_idx,            /* 22U */
+        Hwcal_chan_PORT2_CHAN7_idx,            /* 23U */
+        Hwcal_chan_I_A_TEMP1_idx,              /* 24U */
+        Hwcal_chan_I_A_TLF_5V_idx,             /* 25U */
+        Hwcal_chan_I_A_UZK_idx,                /* 26U */
+        Hwcal_chan_I_A_INT_idx,                /* 27U */
+        Hwcal_chan_I_A_CW_idx,                 /* 28U */
         Hwcal_chan_I_A_BRD_TEMP1_idx,             /* 29U */
-        Hwcal_chan_I_A_PSA_DIAG_2_idx,            /* 30U */
-        Hwcal_chan_I_A_XMR_DIAG_P_idx,            /* 31U */
-        Hwcal_chan_I_A_AMR_SIN_idx,               /* 32U */
-        Hwcal_chan_I_A_3V3_idx,                   /* 33U */
+        Hwcal_chan_I_A_PSA_2_idx,                 /* 30U */
+        Hwcal_chan_I_A_XM_DIAP_idx,               /* 31U */
+        Hwcal_chan_I_A_AIN_idx,                   /* 32U */
+        Hwcal_chan_I_A_idx,                       /* 33U */
         Hwcal_chan_I_A_INV_TEMP_U_idx,            /* 34U */
         Hwcal_chan_AdcMcalDummy4_3_idx,           /* 35U */
-        Hwcal_chan_PORT4_CHAN4_idx,               /* 36U */
-        Hwcal_chan_PORT4_CHAN5_idx,               /* 37U */
-        Hwcal_chan_PORT4_CHAN6_idx,               /* 38U */
-        Hwcal_chan_PORT4_CHAN7_idx,               /* 39U */
-        Hwcal_chan_AdcMcalDummy5_0_idx,           /* 40U */
+        Hwcal_chan_PORT4_CHAN4,               /* 36U */
+        Hwcal_chan_PORT4_CHAN5,               /* 37U */
+        Hwcal_chan_PORT4_CHAN6,               /* 38U */
+        Hwcal_chan_PORT4_CHAN7,               /* 39U */
+        Hwcal_chan_Adc5_0_idx,                /* 40U */
         Hwcal_chan_I_A_1V3_idx,                   /* 41U */
-        Hwcal_chan_I_A_INV_TEMP_V_idx,            /* 42U */
-        Hwcal_chan_I_A_INV_CUR_W_FUSI_idx,        /* 43U */
+        Hwcal_chan_I_A_INV_TEMP_idx,              /* 42U */
+        Hwcal_chan_I_A_INV__WFUSI_idx,            /* 43U */
         Hwcal_chan_I_A_INV_TEE_idx,               /* 44U */
-        Hwcal_chan_I_A_BRD_TEMP2_idx,             /* 45U */
-        Hwcal_chan_I_A_CTRL_VAR_IN_idx,           /* 46U */
-        Hwcal_chan_I_A_XMR_DIAG_N_idx,            /* 47U */
+        Hwcal_chan_I_A_BRD_TE2_idx,               /* 45U */
+        Hwcal_chan_I_A_CL_VAR_IN_idx,             /* 46U */
+        Hwcal_chan_I_A_XMDIAG_N_idx,              /* 47U */
         Hwcal_chan_I_A_AMR_COS_idx,               /* 48U */
-        Hwcal_chan_I_A_TLF_5V_VREF_idx,           /* 49U */
+        Hwcal_chan_I_A_TLF__VREF_idx,             /* 49U */
         Hwcal_chan_I_A_INV_CUR_W_idx,             /* 50U */
         Hwcal_chan_I_A_INV_CUR_U_FUSI_idx,        /* 51U */
         Hwcal_chan_I_A_EM_STAT_TEMP_idx,          /* 52U */
         Hwcal_chan_PORT6_CHAN5_idx,               /* 53U */
         Hwcal_chan_I_A_AMR_DIAG_idx,              /* 54U */
-        Hwcal_chan_I_A_PSA_VDIAG_2_idx,           /* 55U */
+        Hwcal_chan_I_A_VDIAG_2_idx,               /* 55U */
         Hwcal_chan_AdcMcalDummy7_0_idx,           /* 56U */
         Hwcal_chan_I_A_5V_UC_idx,                 /* 57U */
-        Hwcal_chan_I_A_INV_TEMP_W_idx,            /* 58U */
-        Hwcal_chan_I_A_INV_CUR_V_FUSI_idx,        /* 59U */
-        Hwcal_chan_I_A_SENS_CON_TEMP_idx,         /* 60U */
-        Hwcal_chan_PORT7_CHAN5_idx,           /* 61U */
-        Hwcal_chan_PORT7_CHAN6_idx,           /* 62U */
-        Hwcal_chan_I_A_PSA_VDIAG_1_idx,       /* 63U */
-        Hwcal_chan_I_A_U_1V8_HVM_idx,         /* 64U */
-        Hwcal_chan_I_A_U_3V3_HVM_idx,         /* 65U */
-        Hwcal_chan_I_A_U_5V_HVM_idx,          /* 66U */
-        Hwcal_chan_PORT8_CHAN3_idx,           /* 67U */
-        Hwcal_chan_I_A_UZK_MUX_idx,           /* 68U */
-        Hwcal_chan_I_A_U_16V_HVM_idx,         /* 69U */
-        Hwcal_chan_I_A_T_EXC_idx,             /* 70U */
-        Hwcal_chan_I_A_T_HVSUP_idx,           /* 71U */
-        Hwcal_chan_I_T_INV_TEMP_HSS_U_1_idx,  /* 72U */
-        Hwcal_chan_I_T_INV_TEMP_LSS_U_1_idx,  /* 73U */
-        Hwcal_chan_I_T_INV_TEMP_HSS_V_1_idx,  /* 74U */
-        Hwcal_chan_I_T_INV_TEMP_LSS_V_1_idx,  /* 75U */
-        Hwcal_chan_I_T_INV_TEMP_HSS_W_1_idx,  /* 76U */
-        Hwcal_chan_I_T_INV_TEMP_LSS_W_1_idx,  /* 77U */
-        Hwcal_chan_I_T_INV_TEMP_HSS_U_2_idx,  /* 78U */
-        Hwcal_chan_I_T_INV_TEMP_LSS_U_2_idx,  /* 79U */
-        Hwcal_chan_I_T_INV_TEMP_HSS_V_2_idx,  /* 80U */
-        Hwcal_chan_I_T_INV_TEMP_LSS_V_2_idx,  /* 81U */
-        Hwcal_chan_I_T_INV_TEMP_HSS_W_2_idx,  /* 82U */
-        Hwcal_chan_I_T_INV_TEMP_LSS_W_2_idx,  /* 83U */
+        Hwcal_chan_I_A_INV_W_idx,              /* 58U */
+        Hwcal_chan_I_A_INV_CUR_V_idx,          /* 59U */
+        Hwcal_chan_I_A_CONTEMP_idx,            /* 60U */
+        Hwcal_chan_PORT7_CHAN5_,               /* 61U */
+        Hwcal_chan_PORT_CHAN6,                 /* 62U */
+        Hwcal_chan_I_A_PSA_VDIAG_1_idx,        /* 63U */
+        Hwcal_chan_I_A_U_1V8_HVM_idx,          /* 64U */
+        Hwcal_chan_I_A_U_3V3_HVM_idx,          /* 65U */
+        Hwcal_chan_I_A_U_5V_idx,               /* 66U */
+        Hwcal_chan_PORT8_CH3_idx,              /* 67U */
+        Hwcal_chan_I_A_UZK_idx,                /* 68U */
+        Hwcal_chan_I_A_U_16V_HVM_idx,          /* 69U */
+        Hwcal_chan_I_A_T_EXC_idx,              /* 70U */
+        Hwcal_chan_I_A_T_HVSUP_idx,            /* 71U */
+        Hwcal_chan_I_T_INV_TEMPU_1_idx,        /* 72U */
+        Hwcal_chan_I_T_INV_TEMP_LS_idx,        /* 73U */
+        Hwcal_chan_I_T_INV_TEMP_1_idx,         /* 74U */
+        Hwcal_chan_I_T_INV_TEMSS_V_1_idx,      /* 75U */
+        Hwcal_chan_I_T_INV_TEHSS_W_1_idx,      /* 76U */
+        Hwcal_chan_I_T_INV_TEMP_LSS_W_1_idx,   /* 77U */
+        Hwcal_chan_I_T_INV_TE_H_U_2_idx,       /* 78U */
+        Hwcal_chan_I_T_INV_TEMP_L_U_2_idx,     /* 79U */
+        Hwcal_chan_I_T_INV_TE_H_V_2_idx,       /* 80U */
+        Hwcal_chan_I_T_INV_TEMP_LSV_2_idx,     /* 81U */
+        Hwcal_chan_I_T_INV_TEHSS_W_2_idx,      /* 82U */
+        Hwcal_chan_I_T_INV_LSS_W_2_idx,        /* 83U */
         HWCAL_CHAN_MAX_NO,
     }
 
